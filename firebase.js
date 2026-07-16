4057b1292a6d3c913f33d88e3800b9484d2ab83c
RNSRobotics <froggieplaez@gmail.com>
2026-07-16 18:36:32 +1000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2XuxoNtOCFcPwkNVKZCRv3f6rqxjDEGg",
+  authDomain: "geopad-46a21.firebaseapp.com",
+  projectId: "geopad-46a21",
+  storageBucket: "geopad-46a21.firebasestorage.app",
+  messagingSenderId: "1076056608974",
+  appId: "1:1076056608974:web:ea403915b0faa29e9fc09f",
+  measurementId: "G-EECLGH2DE7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
